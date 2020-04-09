4e7501691dfdd0d48141830a1449b55698f23f62
Miguel Lugo <miguellugoml@gmail.com>
2020-04-09 17:08:48 +0200

    Agregando todo el Proyecto PrimerCRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Empleado } from './modelo/empleado';
+import { Identifiers } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PrimerCRUD';
+  title = 'elPrimerCRUD';
+  EmpleadoArreglo:Empleado[]=[
+    {id:1, nombre: "Miguel", pais: "Vzla"},
+    {id:2, nombre: "Alcira", pais: "Col"},
+    {id:3, nombre: "Valeria", pais: "Esp"},
+    {id:4, nombre: "Gabriel", pais: "Esp"},
+  ];
+
+  EmpleadoSelect:Empleado = new Empleado();
+  
+
+  agregarEditar(){
+
+    if(this.EmpleadoSelect.id === 0){
+
+      this.EmpleadoSelect.id = this.EmpleadoArreglo.length + 1;
+      this.EmpleadoArreglo.push(this.EmpleadoSelect);
+    }
+    
+    this.EmpleadoSelect = new Empleado();
+  }
+  editar(empleado:Empleado){
+    this.EmpleadoSelect=empleado;
+
+  }
+  borrar(){
+     
+    if(confirm("Seguro que desea eliminar?")){
+
+        this.EmpleadoArreglo = this.EmpleadoArreglo.filter(x => x != this.EmpleadoSelect);
+
+        for (let i = 0; i < this.EmpleadoArreglo.length; i++) {
+          this.EmpleadoArreglo[i].id=i+1;
+          
+        }
+        this.EmpleadoSelect = new Empleado();
+    }
+  
+  }
+
 }
